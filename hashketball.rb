4ce937535cc0f2af3a571b5e7e5e87d6bb61b84c
Justine Manaloto <justine.kim.manaloto@gmail.com>
2020-09-28 21:31:43 +0000

    Done.

--- hashketball.rb
+++ hashketball.rb
@@ -1,1 +1,21 @@
 # Write your code here!
+
+def hashketball(game_hash)
+  game_hash = {}
+  game_hash = {
+    :home =>{
+      :team_name => {
+        
+      },
+      :colors => {
+        
+      },
+      :players => {
+        
+      }
+      
+  },
+    :away =>{
+  }
+  }
+  end
